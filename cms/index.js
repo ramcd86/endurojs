a4bebfebb6d039620b380f61ed99c8403e58330e
unknown <ra_mcd@yahoo.com>
2019-05-27 21:46:40 +0100

    Ongoing work.

--- cms/index.js
+++ cms/index.js
@@ -9,43 +9,57 @@
 			$published_on_type: 'date',
 			header: 'BLOCK WIDE',
 			tagline: 'NA',
-			display_options: '@@global.display.full'
+			display_options: '@@global.display.full',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/aUetK.jpg'
 		},
 		{
 			$published_on_type: 'date',
 			header: 'BLOCK HALF',
 			tagline: 'NA',
-			display_options: '@@global.display.half'
+			display_options: '@@global.display.half',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/snxqe.jpg'
 		},
 		{
 			$published_on_type: 'date',
 			header: 'BLOCK HALF',
 			tagline: 'NA',
-			display_options: '@@global.display.half'
+			display_options: '@@global.display.half',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/wCmsm.jpg'
 		},
 		{
 			$published_on_type: 'date',
 			header: 'BLOCK QUARTER',
 			tagline: 'NA',
-			display_options: '@@global.display.quarter'
+			display_options: '@@global.display.quarter',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/QVy2t.jpg'
 		},
 		{
 			$published_on_type: 'date',
 			header: 'BLOCK QUARTER',
 			tagline: 'NA',
-			display_options: '@@global.display.quarter'
+			display_options: '@@global.display.quarter',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/6C5oh.jpg'
 		},
 		{
 			$published_on_type: 'date',
 			header: 'BLOCK QUARTER',
 			tagline: 'NA',
-			display_options: '@@global.display.quarter'
+			display_options: '@@global.display.quarter',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/JnyiT.jpg'
 		},
 		{
 			$published_on_type: 'date',
 			header: 'BLOCK QUARTER',
 			tagline: 'NA',
-			display_options: '@@global.display.quarter'
+			display_options: '@@global.display.quarter',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/KGIsM.jpg'
 		}
 	],
 	so_much_more_block: {
