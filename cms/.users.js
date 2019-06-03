adc4298a0cc8a371d3abf71d83c524f9ba17b907
unknown <ra_mcd@yahoo.com>
2019-06-03 22:12:31 +0100

    Ongoing work

--- cms/.users.js
+++ cms/.users.js
@@ -1,11 +1,11 @@
 ({
 	users: [
 		{
-			username: 'admin',
+			username: 'ramcd86',
 			tags: [],
-			salt: 'ec465e7fbc5742bd158707cdb2c4208b',
-			hash: 'a84af43daa6d64c442b28a5040074117cba7b5f68d4b316117f0ffc0e34990c0',
-			user_created_timestamp: 1558214346247
+			salt: 'f52603e8894b82f6834ddc3ff59d611a',
+			hash: '4aea2cad77d12eec2fa583508c82ae52366652484e75d631deddc35962639500',
+			user_created_timestamp: 1559590670684
 		}
 	]
 })
