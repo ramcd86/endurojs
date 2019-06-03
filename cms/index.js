47661f685c69425706e2b1aaaf3287a2056c7c8f
@McDermott <ra_mcd@yahoo.com>
2019-06-03 14:36:01 +0100

    Ongoing work.

--- cms/index.js
+++ cms/index.js
@@ -6,74 +6,24 @@
 	},
 	instruction_points: [
 		{
-			header: 'An Article About Things.',
-			tagline: 'A little article about a few things, chief. What else?',
+			header: 'TEMPLATE POST, COPY THIS POST.',
+			tagline: 'TEMPLATE POST, COPY THIS POST.',
 			$published_on_type: 'date',
 			published_on: 'Thursday, 16 May, 2019',
-			display_options: '@@global.display.half',
+			display_options: '@@global.display.hidden',
 			$background_image_type: 'image',
 			background_image: 'https://i.imgur.com/aUetK.jpg',
 			$published_on_value: '2019-05-16'
 		},
 		{
-			header: 'Where The Hills Are',
-			tagline: 'Discussing the hills, where they are, and how we\'ll get there.',
+			header: 'Pushing The Wheel Not Good Enough For You? Pull This Lever.',
+			tagline: 'On the Right\'s moron \'Learn to Code\' mantra, from someone who works in Development.',
 			$published_on_type: 'date',
-			published_on: 'Wednesday, 22 May, 2019',
-			display_options: '@@global.display.half',
-			$background_image_type: 'image',
-			background_image: 'https://i.imgur.com/snxqe.jpg',
-			$published_on_value: '2019-05-22'
-		},
-		{
-			header: 'His Apple Orchard',
-			tagline: 'How has apple farming affected us? I don\'t know.',
-			$published_on_type: 'date',
-			published_on: 'Sunday, 19 May, 2019',
+			published_on: 'Monday, 03 June, 2019',
 			display_options: '@@global.display.full',
 			$background_image_type: 'image',
-			background_image: 'https://i.imgur.com/wCmsm.jpg',
-			$published_on_value: '2019-05-19'
-		},
-		{
-			header: 'The Magic Of Time',
-			tagline: 'An end to the theory that time is magical.',
-			$published_on_type: 'date',
-			published_on: 'Saturday, 11 May, 2019',
-			display_options: '@@global.display.quarter',
-			$background_image_type: 'image',
-			background_image: 'https://i.imgur.com/QVy2t.jpg',
-			$published_on_value: '2019-05-11'
-		},
-		{
-			header: 'I Went A Wheeling',
-			tagline: 'Discussing cycling, and how it will green the world.',
-			$published_on_type: 'date',
-			published_on: 'Friday, 17 May, 2019',
-			display_options: '@@global.display.quarter',
-			$background_image_type: 'image',
-			background_image: 'https://i.imgur.com/6C5oh.jpg',
-			$published_on_value: '2019-05-17'
-		},
-		{
-			header: 'The Last Rights of Grove',
-			tagline: 'A insight into the last rights of Grove, Arthur.',
-			$published_on_type: 'date',
-			published_on: 'Wednesday, 15 May, 2019',
-			display_options: '@@global.display.quarter',
-			$background_image_type: 'image',
-			background_image: 'https://i.imgur.com/JnyiT.jpg',
-			$published_on_value: '2019-05-15'
-		},
-		{
-			header: 'The Hills of Navarone',
-			tagline: 'On the hills of Navarone we ask, is it too much?',
-			$published_on_type: 'date',
-			published_on: 'Monday, 13 May, 2019',
-			display_options: '@@global.display.quarter',
-			$background_image_type: 'image',
-			background_image: 'https://i.imgur.com/KGIsM.jpg',
-			$published_on_value: '2019-05-13'
+			background_image: '/remote/direct_uploads/1559566655_js.jpg',
+			$published_on_value: '2019-06-03'
 		}
 	],
 	so_much_more_block: {
