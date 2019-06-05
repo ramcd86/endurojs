c204824cc417c0b01f214bc0afa88fb65cb49044
@McDermott <ra_mcd@yahoo.com>
2019-06-05 21:13:21 +0100

    Ongoing work.

--- cms/index.js
+++ cms/index.js
@@ -1,17 +1,6 @@
 ({
 	instruction_points: [
 		{
-			header: 'TEMPLATE POST, COPY THIS POST.',
-			tagline: 'TEMPLATE POST, COPY THIS POST.',
-			$published_on_type: 'date',
-			published_on: 'Thursday, 16 May, 2019',
-			display_options: '@@global.display.hidden',
-			link: '/blog/_post',
-			$background_image_type: 'image',
-			background_image: 'https://i.imgur.com/aUetK.jpg',
-			$published_on_value: '2019-05-16'
-		},
-		{
 			header: 'Pushing The Wheel Not Good Enough For You? Pull This Lever.',
 			tagline: 'On the Right\'s moron \'Learn to Code\' mantra, from someone who works in Development.',
 			$published_on_type: 'date',
@@ -21,6 +10,105 @@
 			$background_image_type: 'image',
 			background_image: '/remote/direct_uploads/1559566655_js.jpg',
 			$published_on_value: '2019-06-03'
+		},
+		{
+			header: 'TEMPLATE POST, COPY THIS POST.',
+			tagline: 'TEMPLATE POST, COPY THIS POST.',
+			$published_on_type: 'date',
+			published_on: 'Thursday, 16 May, 2019',
+			display_options: '@@global.display.half',
+			link: '/blog/_post',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/aUetK.jpg',
+			$published_on_value: '2019-05-16'
+		},
+		{
+			header: 'TEMPLATE POST, COPY THIS POST.',
+			tagline: 'TEMPLATE POST, COPY THIS POST.',
+			$published_on_type: 'date',
+			published_on: 'Thursday, 16 May, 2019',
+			display_options: '@@global.display.half',
+			link: '/blog/_post',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/aUetK.jpg',
+			$published_on_value: '2019-05-16'
+		},
+		{
+			header: 'TEMPLATE POST, COPY THIS POST.',
+			tagline: 'TEMPLATE POST, COPY THIS POST.',
+			$published_on_type: 'date',
+			published_on: 'Thursday, 16 May, 2019',
+			display_options: '@@global.display.third',
+			link: '/blog/_post',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/aUetK.jpg',
+			$published_on_value: '2019-05-16'
+		},
+		{
+			header: 'TEMPLATE POST, COPY THIS POST.',
+			tagline: 'TEMPLATE POST, COPY THIS POST.',
+			$published_on_type: 'date',
+			published_on: 'Thursday, 16 May, 2019',
+			display_options: '@@global.display.third',
+			link: '/blog/_post',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/aUetK.jpg',
+			$published_on_value: '2019-05-16'
+		},
+		{
+			header: 'TEMPLATE POST, COPY THIS POST.',
+			tagline: 'TEMPLATE POST, COPY THIS POST.',
+			$published_on_type: 'date',
+			published_on: 'Thursday, 16 May, 2019',
+			display_options: '@@global.display.third',
+			link: '/blog/_post',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/aUetK.jpg',
+			$published_on_value: '2019-05-16'
+		},
+		{
+			header: 'TEMPLATE POST, COPY THIS POST.',
+			tagline: 'TEMPLATE POST, COPY THIS POST.',
+			$published_on_type: 'date',
+			published_on: 'Thursday, 16 May, 2019',
+			display_options: '@@global.display.quarter',
+			link: '/blog/_post',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/aUetK.jpg',
+			$published_on_value: '2019-05-16'
+		},
+		{
+			header: 'TEMPLATE POST, COPY THIS POST.',
+			tagline: 'TEMPLATE POST, COPY THIS POST.',
+			$published_on_type: 'date',
+			published_on: 'Thursday, 16 May, 2019',
+			display_options: '@@global.display.quarter',
+			link: '/blog/_post',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/aUetK.jpg',
+			$published_on_value: '2019-05-16'
+		},
+		{
+			header: 'TEMPLATE POST, COPY THIS POST.',
+			tagline: 'TEMPLATE POST, COPY THIS POST.',
+			$published_on_type: 'date',
+			published_on: 'Thursday, 16 May, 2019',
+			display_options: '@@global.display.quarter',
+			link: '/blog/_post',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/aUetK.jpg',
+			$published_on_value: '2019-05-16'
+		},
+		{
+			header: 'TEMPLATE POST, COPY THIS POST.',
+			tagline: 'TEMPLATE POST, COPY THIS POST.',
+			$published_on_type: 'date',
+			published_on: 'Thursday, 16 May, 2019',
+			display_options: '@@global.display.quarter',
+			link: '/blog/_post',
+			$background_image_type: 'image',
+			background_image: 'https://i.imgur.com/aUetK.jpg',
+			$published_on_value: '2019-05-16'
 		}
 	]
 })
